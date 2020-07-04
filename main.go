d7971280534180d911d93e1409cdc896500f7c8f
NathanielRand <nathanieljrand@gmail.com>
2020-07-04 01:11:49 -0400

    Update design

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"log"
+	"os"
 
 	"github.com/NathanielRand/portfolio/views"
-
 	"github.com/gorilla/mux"
+	"github.com/gorilla/schema"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
 var (
@@ -19,6 +23,12 @@ var (
 	hobbiesView  *views.View
 	travelsView  *views.View
 )
+
+type ContactForm struct {		
+	Email     string `schema:"email"`
+	Subject   string `schema:"subject"`
+	Message   string `schema:"message"`
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -84,6 +94,57 @@ func travels(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func parseForm(r *http.Request, dst interface{}) error {
+	// Parse the HTML form that was submitted
+	// and store the data in the PostForm field of the http.Request.
+	if err := r.ParseForm(); err != nil {
+		return err
+	}
+
+	// Initialize our decoder.
+	dec := schema.NewDecoder()	
+
+	// Call the Decode method on our decode and
+	// pass in the SignupForm as our destination and
+	// r.PostForm as our data source. Check for errors.
+	if err := dec.Decode(dst, r.PostForm); err != nil {
+		return err
+	}
+
+	// Return nil when no errors occur.
+	return nil
+}
+
+func contactEmail(w http.ResponseWriter, r *http.Request) {	
+	var form ContactForm
+	if err := parseForm(r, &form); err != nil {
+		panic(err)
+	}
+	// form.Email
+	// form.Subject
+	// form.Message
+
+	from := mail.NewEmail("Nathaniel Rand", "nathanieljrand@gmail.com")
+	subject := form.Subject
+	to := mail.NewEmail("Nathaniel Rand", "nathanieljrand@gmail.com")
+	plainTextContent := form.Message
+	htmlContent := "<strong>form.Message</strong>"
+	
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+	
+	response, errSend := client.Send(message)
+	if errSend != nil {
+		log.Println(errSend)
+	} else {
+		fmt.Println(response.StatusCode)
+		fmt.Println(response.Body)
+		fmt.Println(response.Headers)
+		fmt.Println(form.Email)
+	}
+	
+}
+
 func faviconHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "/favicon.ico")
 }
@@ -113,6 +174,7 @@ func main() {
 	// Define paths with assigned functions
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/contact", contactEmail).Methods("POST")
 	r.HandleFunc("/socials", socials)
 	r.HandleFunc("/projects", projects)
 	r.HandleFunc("/skills", skills)
